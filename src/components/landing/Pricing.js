247474da1466cdda119891f48cc3634915d3bb8c
Filip Tsolov <filip.tsolov@code.berlin>
2021-06-22 11:07:08 +0300

    added pricing section

--- src/components/landing/Pricing.js
+++ src/components/landing/Pricing.js
@@ -5,31 +5,49 @@ const Pricing = () => {
   return (
     <div id={"pricing"}>
       <HeadingComponent title={"Pricing"} description={"Which price plan fits you best?"} />
-      <div>
-        <div className="pricing-card">
+      <div className={"pricing-card-container"}>
+        <div className="pricing-card card-one">
           <div className="pricing-title">
-            <h2></h2>
-            <h3></h3>
+            <h2>Basic</h2>
+            <h3><span className={"price-highlight"}>99€</span> pro Monat </h3>
           </div>
-          <hr />
+          <hr className={"hr-styled"} />
           <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li>1 Seite (+ Pflichtangaben EU)</li>
+            <li>Bis zu 2 GB Inhalte</li>
+            <li>Website in 10 days</li>
+            <li>1 Domain</li>
           </ul>
         </div>
-        <div className="pricing-card">
+        <div className="pricing-card card-two">
           <div className="pricing-title">
-            <h2></h2>
-            <h3></h3>
+            <h2>Pro</h2>
+            <h3><span className={"price-highlight"}>149€</span> pro Monat</h3>
           </div>
+          <hr className={"hr-styled"} />
+          <ul>
+            <li>3 Domains</li>
+            <li>5 E-Mail Adressen</li>
+            <li>Bis zu 3 Seiten(+ Pflichtangaben EU)</li>
+            <li>Bis zu 8 GB Inhalte</li>
+            <li>1 monatliches Review</li>
+            <li>DSGVO-Beratung</li>
+            <li>Website in 3 Tagen</li>
+          </ul>
         </div>
-        <div className="pricing-card">
+        <div className="pricing-card card-three">
           <div className="pricing-title">
-            <h2></h2>
-            <h3></h3>
+            <h2>Enterprise</h2>
+            <h3><span className={"price-highlight"}>229€</span> pro monat</h3>
           </div>
+          <hr className={"hr-styled"} />
+          <ul>
+            <li>5 Domains</li>
+            <li>10 E-Mail Adressen</li>
+            <li>25GB+ Inhalte</li>
+            <li>Website in 7 days</li>
+            <li>Hotline Support</li>
+          </ul>
         </div>
       </div>
     </div>
