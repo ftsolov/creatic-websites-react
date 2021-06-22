034ad9495728d145818afd8e98f0c03e970caf0e
Filip Tsolov <filip.tsolov@code.berlin>
2021-06-22 10:04:27 +0300

    fixed contact component styling

--- src/components/landing/ContactEmail.js
+++ src/components/landing/ContactEmail.js
@@ -6,32 +6,38 @@ class ContactEmail extends React.Component {
     super(props);
     this.state = {
       text: "contact@creaticwebsites.com",
+      color: "var(--white)",
     };
   }
 
   handleHover = () => {
-    this.setState({ text: "Click to copy email!" });
+    this.setState({ color: "var(--light-grey)", text: "Click to copy email!" });
   };
 
   handleHoverLeave = () => {
-    this.setState({ text: "contact@creaticwebsites.com" });
+    this.setState({
+      color: "var(--white)",
+      text: "contact@creaticwebsites.com",
+    });
   };
 
   handleClick = () => {
-    this.setState({ text: "Email copied to clipboard!" });
+    this.setState({
+      color: "var(--light-grey)",
+      text: "Email copied to clipboard!",
+    });
     navigator.clipboard.writeText("contact@creaticwebsites.com");
   };
 
   render() {
     return (
-      <div className={"email"}>
-        <h2
-          onMouseEnter={this.handleHover}
-          onMouseLeave={this.handleHoverLeave}
-          onMouseDown={this.handleClick}
-        >
-          {this.state.text}
-        </h2>
+      <div
+        className={"email"}
+        onMouseEnter={this.handleHover}
+        onMouseLeave={this.handleHoverLeave}
+        onMouseDown={this.handleClick}
+      >
+        <h2 style={{ color: this.state.color }}>{this.state.text}</h2>
         <h2>@</h2>
       </div>
     );
